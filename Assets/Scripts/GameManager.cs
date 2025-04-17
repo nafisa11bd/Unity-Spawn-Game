74640b5147a074990a9e59339fff9ef0bbd92ac3
Nafisa Sabnam Shama <shama@Charlies-Air.dhcp.wfu.edu>
2025-04-17 17:38:54 -0400

    added enemy killed counter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,12 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    public bool _gameover=false;
-    
+    public bool _gameover = false;
+    private int _enemyKillCount = 0;
+
+    public void IncrementKillCount()
+    {
+        _enemyKillCount++;
+        Debug.Log($"Enemy Kill Count: {_enemyKillCount}");
+    }
 }
